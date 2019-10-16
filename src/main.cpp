223bcb9e1ad0cae60f386c73e247945df572f07c
microshine <microshine@mail.ru>
2019-10-16 11:24:43 +0300

    Add segmentation catch code

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,36 @@
 #include "const.h"
 #include "node.h"
 
+#include <signal.h>
+#include <execinfo.h>
+
+void handler(int sig) {
+  void *array[20];
+  size_t size;
+
+  // get void*'s for all entries on the stack
+  size = backtrace(array, 20);
+
+  // print out all the frames to stderr
+  fprintf(stderr, "Error: signal %d:\n", sig);
+  char** strings = backtrace_symbols(array, size);
+
+	if (strings) {
+		for (size_t i = 0; i < size; i++) {
+			fprintf(stderr, "  %s\n", strings[i]);
+		}
+		free(strings);
+	}
+
+  exit(1);
+}
+
+
+
 NAN_MODULE_INIT(init)
 {
+	signal(SIGSEGV, handler);
+
 	Nan::HandleScope scope;
 
 	WPKCS11::Init(target);
